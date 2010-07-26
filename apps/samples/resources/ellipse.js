a1c78ba8d73d27d6de823c1f582ad5167b7eb322
Evin Grano <etgryphon@sproutcore.com>
2010-07-26 16:05:18 -0400

    Latest round of examples in VML

--- apps/samples/resources/ellipse.js
+++ apps/samples/resources/ellipse.js
@@ -13,21 +13,31 @@ Samples.ellipsePage = SC.Page.design({
     
     basic: Sai.CanvasView.design({
       layout: { left: 25, top: 20, height: 200, width: 465 },
-      childElements: 'ellipse1 center'.w(),
+      childElements: 'rect1 ellipse1 center'.w(),
       backgroundColor: 'yellow',
       
       ellipse1: Sai.Ellipse.create({
-        x: 225,
+        x: 232,
         y: 100,
         radiusX: 150,
         radiusY: 50,
         fill: 'lightblue',
         stroke: 'purple',
-        strokeWidth: 15
+        strokeWidth: 10
+      }),
+      
+      rect1: Sai.Rectangle.create({
+        x: 82,
+        y: 50,
+        width: 300,
+        height: 100,
+        fill: 'lightblue',
+        stroke: 'purple',
+        strokeWidth: 1
       }),
       
       center: Sai.Circle.create({
-        x: 225,
+        x: 232,
         y: 100,
         radius: 5,
         fill: 'black'
